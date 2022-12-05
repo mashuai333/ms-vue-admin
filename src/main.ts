2d73c83c2d4f57cdaedbea2aaef7662d10ff86ca
mashuai <mas@eadage.com>
2022-12-05 10:39:13 +0800

    增加vueuse/motion

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { MotionPlugin } from "@vueuse/motion";
+
 import './style.css'
 import App from './App.vue'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(MotionPlugin)
+app.mount('#app')
