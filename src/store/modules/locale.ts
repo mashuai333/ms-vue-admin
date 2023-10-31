86145cdee01b85a07c9d5ac11aa448ba39dc6fae
马帅 <953096410@qq.com>
2023-10-31 17:46:26 +0800

    chore: 升级package.json

--- src/store/modules/locale.ts
+++ src/store/modules/locale.ts
@@ -1,8 +1,10 @@
 import { defineStore } from 'pinia'
 import { pinia } from '../index'
 // element-plus 国际化
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
-import en from 'element-plus/lib/locale/lang/en'
+// import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+// import en from 'element-plus/lib/locale/lang/en'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import en from 'element-plus/dist/locale/en.mjs'
 
 import { useCache } from '@/hooks/web/useCache'
 import { LocaleDropdownType } from '@/types/localeDropdown'
